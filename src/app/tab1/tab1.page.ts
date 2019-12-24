eb9f6e8a1a6bb164c2d59d10a65cf4a318ba55cd
Rajeshkannan Palvannan <rajeshkannan@heptagon.in>
2019-12-24 19:07:44 +0530

    Tabs Psh

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,19 +8,46 @@ import { CalculationService } from '../services/calculation.service';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+
   calculation: any;
   numbers: any;
   selectedNumberIndex: any;
+  totalNumberPairs: any;
+  digit: any;
+  sign: any;
+
   constructor() {
     this.selectedNumberIndex = 0;
+    this.totalNumberPairs = 18;
+    this.digit = 3;
+    this.sign = 'plus';
+
     this.calculation = new CalculationService();
   }
   ngOnInit() {
-    this.numbers = this.calculation.getDynamicNumbers('plus', 18, 3);
-    console.log(this.numbers);
+    this.numbers = this.calculation.getDynamicNumbers(
+      this.sign,
+      this.totalNumberPairs,
+      this.digit
+    );
+    this.selectedNumberIndex = Math.round(Math.random()* (this.numbers.filter(num=> num.choosen !== true).length));
   }
+
+  changeNumber() {
+    this.selectedNumberIndex = Math.round(Math.random() * this.totalNumberPairs);
+    do {
+        this.selectedNumberIndex = Math.round(Math.random() * this.totalNumberPairs);
+        console.log(this.selectedNumberIndex);
+    }
+    while(this.numbers[this.selectedNumberIndex].choosen);
+  }
+
   checkAnswer(index){
-      console.log(index);
       this.numbers[index]['choosen'] = this.numbers[index]['total'] === this.numbers[this.selectedNumberIndex]['total'];
+      if(this.numbers[index]['choosen']) {
+        this.changeNumber()
+      } else {
+        console.log("Wrong Answer");
+      }
   }
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// Custom Services
+import { CalculationService } from '../services/calculation.service';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +9,45 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  calculation: any;
+  numbers: any;
+  selectedNumberIndex: any;
+  totalNumberPairs: any;
+  digit: any;
+  sign: any;
 
+  constructor() {
+    this.selectedNumberIndex = 0;
+    this.totalNumberPairs = 18;
+    this.digit = 3;
+    this.sign = 'multiply';
+
+    this.calculation = new CalculationService();
+  }
+  ngOnInit() {
+    this.numbers = this.calculation.getDynamicNumbers(
+      this.sign,
+      this.totalNumberPairs,
+      this.digit
+    );
+    this.selectedNumberIndex = Math.round(Math.random()* (this.numbers.filter(num=> num.choosen !== true).length));
+  }
+
+  changeNumber() {
+    this.selectedNumberIndex = Math.round(Math.random() * this.totalNumberPairs);
+    do {
+        this.selectedNumberIndex = Math.round(Math.random() * this.totalNumberPairs);
+        console.log(this.selectedNumberIndex);
+    }
+    while(this.numbers[this.selectedNumberIndex].choosen);
+  }
+
+  checkAnswer(index){
+      this.numbers[index]['choosen'] = this.numbers[index]['total'] === this.numbers[this.selectedNumberIndex]['total'];
+      if(this.numbers[index]['choosen']) {
+        this.changeNumber()
+      } else {
+        console.log("Wrong Answer");
+      }
+  }
 }
